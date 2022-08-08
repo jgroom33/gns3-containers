7ec71bdd93135dc559ad7cd7a47a5f3495be3878
Jeff Groom <jgroom@ciena.com>
2022-08-08 12:31:51 -0600

    start

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+ifconfig eth0 192.168.122.3
+
+
+leases:
+cat /var/lib/dhcp/dhcpd.leases
--- net_toolbox/starter.sh
+++ net_toolbox/starter.sh
@@ -3,6 +3,7 @@
 /etc/init.d/rsyslog start >/dev/null 2>/dev/null
 /etc/init.d/nginx start >/dev/null 2>/dev/null
 /etc/init.d/vsftpd start >/dev/null 2>/dev/null
+/etc/init.d/isc-dhcp-server start >/dev/null 2>/dev/null
 /etc/init.d/tftpd-hpa start >/dev/null 2>/dev/null
 SNMPD_PID=`cat /run/snmpd.pid`
 /etc/init.d/snmpd stop >/dev/null 2>/dev/null
@@ -15,4 +16,3 @@ clear
 # Launching shell
 cd
 exec bash -i
-
